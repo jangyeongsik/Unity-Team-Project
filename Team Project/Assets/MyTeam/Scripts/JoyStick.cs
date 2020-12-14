b2d37afcf0145e7c7ce2f3f09b3f78887e1a1288
dudqls5630 <dudqls5630@gmail.com>
2020-12-14 14:06:36 +0900

    조이스틱 작업

---
+++ Team Project/Assets/MyTeam/Scripts/JoyStick.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeReference, Range(10,150)]
+    private float leverRange;
+
+    private Vector2 inputDirection;
+    private bool isInput;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControllJoystickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControllJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        inputDirection = Vector2.zero;
+    }
+
+    private void ControllJoystickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    public float Horizontal()
+    {
+        return inputDirection.x;
+    }
+    public float Vertical()
+    {
+        return inputDirection.y;
+    }
+
+}
---
+++ Team Project/Assets/MyTeam/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public JoyStick joyStick;
+    private float moveSpeed = 15;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = joyStick.Horizontal();
+        float y = joyStick.Vertical();
+
+        Vector3 dir = new Vector3(x, 0, y);
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+    }
+}
