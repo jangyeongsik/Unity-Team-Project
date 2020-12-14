a3628efbbfb750519eec6554361069ef5cd8cb16
jangyeongsik <sik4909@daum.net>
2020-12-14 10:31:02 +0900

    고정 해상도

---
+++ Team Project/Assets/MyTeam/Scripts/FixScreenSize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixScreenSize : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(1600, 900, true);
+    }
+}
