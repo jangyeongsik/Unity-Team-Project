83b1afcc4b457add5e1db695a519418ffcad7003
Cyril Kim <jaykim1994@gmail.com>
2020-12-15 15:32:02 +0900

    시네머신 연습 (카메라 줌인/아웃 + 플레이어 따라 움직이기 + 프리룩 카메라, 궤도공전 카메라 연습)

---
+++ Team Project/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float ZoomSpeed;
+
+    Cinemachine.CinemachineFollowZoom offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = GetComponent<Cinemachine.CinemachineFollowZoom>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float zoomDir = Input.GetAxis("Mouse ScrollWheel");
+        offset.m_Width -= zoomDir * ZoomSpeed * 2.0f;
+        if (offset.m_Width <= 0f) offset.m_Width = 0f;
+        if (offset.m_Width >= 22f) offset.m_Width = 22f;
+    }
+}
