68a872657e5df6ea3aba8490c99a19bc0d1ba709
Cyril Kim <jaykim1994@gmail.com>
2020-12-15 16:40:29 +0900

    Dolly Camera 사용해봄 + 스크립트로 virtual camera 간 전환처리?

---
+++ Team Project/Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+    [SerializeField] Cinemachine.CinemachineVirtualCamera[] vCamera;
+
+    void Start()
+    {
+        vCamera[0].enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            foreach(var cam in vCamera)
+            {
+                cam.enabled = false;
+            }
+            vCamera[0].enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            foreach (var cam in vCamera)
+            {
+                cam.enabled = false;
+            }
+            vCamera[1].enabled = true;
+        }
+    }
+}
