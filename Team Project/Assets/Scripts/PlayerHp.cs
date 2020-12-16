94e6a7cf7e23f49ac41633edacdc03576ff33bf3
jangyeongsik <sik4909@daum.net>
2020-12-16 13:14:32 +0900

    플레이어 hp베이스

---
+++ Team Project/Assets/Scripts/PlayerHp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHp : MonoBehaviour
+{
+    private Slider slider;             //슬라이더
+    public int maxHp;           //최대 체력
+    public int currentHp;       //현재 체력
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        slider.value = (float)currentHp / (float)maxHp;
+    }
+}
