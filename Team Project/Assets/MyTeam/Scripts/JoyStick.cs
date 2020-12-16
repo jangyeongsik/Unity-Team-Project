03e77e6f15acddaaf6af485c104435479a79a4b5
dudqls5630 <dudqls5630@gmail.com>
2020-12-16 15:20:00 +0900

    조이스틱

--- Team Project/Assets/MyTeam/Scripts/JoyStick.cs
+++ Team Project/Assets/MyTeam/Scripts/JoyStick.cs
@@ -6,24 +6,28 @@ using UnityEngine.EventSystems;
 public class JoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField]
-    private RectTransform lever;
-    private RectTransform rectTransform;
+    private GameObject BaseLever;       //뒷배경
+    private Vector3 basePos;        
+   // private RectTransform lever;
+    //private RectTransform rectTransform;
 
     [SerializeReference, Range(10,150)]
     private float leverRange;
 
-    private Vector2 inputDirection;
+    private Vector3 inputDirection;
     private bool isInput;
 
     private void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
+        //rectTransform = GetComponent<RectTransform>();
+        basePos = BaseLever.transform.position;
+       // lever.anchoredPosition = Vector2.zero;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        ControllJoystickLever(eventData);
-        isInput = true;
+        //ControllJoystickLever(eventData);
+        //isInput = true;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -33,17 +37,29 @@ public class JoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        lever.anchoredPosition = Vector2.zero;
+       // lever.anchoredPosition = Vector2.zero;
         isInput = false;
         inputDirection = Vector2.zero;
     }
 
     private void ControllJoystickLever(PointerEventData eventData)
     {
-        var inputPos = eventData.position - rectTransform.anchoredPosition;
-        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
-        lever.anchoredPosition = inputVector;
-        inputDirection = inputVector / leverRange;
+        //var inputPos = eventData.position - rectTransform.anchoredPosition;
+        //var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        // lever.anchoredPosition = inputVector;
+        //inputDirection = inputVector / leverRange;
+
+        inputDirection = (Vector3)eventData.position - basePos;
+        inputDirection.Normalize();
+        var dist = Vector3.Distance(basePos, eventData.position);
+        if(dist < leverRange)
+        {
+            transform.position = basePos + inputDirection * dist;
+        }
+        else
+        {
+            transform.position = basePos + inputDirection * leverRange;
+        }
     }
 
     public float Horizontal()
