c2df78785465f385735efcb9563390a6ca2a5241
유인 한 <gksyy123@naver.com>
2020-12-18 00:20:48 +0900

    1. 쿼터뷰식, 간단한 플레이어 큐브 + 카메라 움직임 2. 예시 NPC대화창

---
+++ Team Project/Assets/MyTeam/Scenes/NPC/Scripts/IsoMetric.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IsoMetric : MonoBehaviour
+{
+    [SerializeField] private float offsetY = 4.0f;
+    public GameObject player;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, player.transform.position.z);
+    }
+}
---
+++ Team Project/Assets/MyTeam/Scenes/NPC/Scripts/IsoMetricController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoMetricController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 4f;
+    Vector3 forward, right;
+
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0.0f;
+        forward = Vector3.Normalize(forward);
+
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 RightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 ForwardMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 FinalMovement = ForwardMovement + RightMovement;
+        Vector3 direction = Vector3.Normalize(FinalMovement);
+
+        if(direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            transform.position += FinalMovement;
+        }
+    }
+}
