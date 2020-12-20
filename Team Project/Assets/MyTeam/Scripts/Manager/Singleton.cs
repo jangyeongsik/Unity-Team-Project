b0e4649ce12162d8c5493c848d527117d27ce6a4
전영은 <wjsduddms128@gmail.com>
2020-12-20 18:20:07 +0900

    NPCTalkCSVReader

---
+++ Team Project/Assets/MyTeam/Scripts/Manager/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> where T : class, new()
+{
+    static T s_instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (s_instance == null)
+            {
+                CreateInstance();
+            }
+
+            return s_instance;
+        }
+    }
+
+    static void CreateInstance()
+    {
+        if (s_instance == null)
+        {
+            s_instance = new T();
+        }
+    }
+}
---
+++ Team Project/Assets/MyTeam/Scripts/talkdataTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talkdataTest : SingletonMonobehaviour<talkdataTest>
+{
+    public List<NPCReader.NPCTalk> data;
+    private void Start()
+    {
+        data = CSVReaderNPC.CSVReaderNPC.FileRead("talkdata");
+        for (int i = 0; i < data.Count; i++)
+        {
+            Debug.Log(data[i].id + "\n" + data[i].name + "\n");
+            for (int l = 0; l < data[i].talk.Count; l++)
+            {
+                Debug.Log(data[i].talk[l] + " ");
+            }
+        }
+    }
+}
