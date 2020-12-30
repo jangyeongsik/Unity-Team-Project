7d536ebbc453c89af637fdcc043552f0c8653312
전영은 <wjsduddms128@gmail.com>
2020-12-30 16:42:24 +0900

    enemy

--- Team Project/Assets/MyTeam/Scripts/InGame/Enemy/Monster.cs
+++ Team Project/Assets/MyTeam/Scripts/InGame/Enemy/Monster.cs
@@ -9,18 +9,14 @@ public class Monster : character
 {
     public int skillid;                 //스킬 아이디
     public float counter_reslstance;    //카운터 저항
-    public float user_aware_distance;   //유저 인식 거리
+    public float attack_aware_distance;   //공격 거리
 
     public State.MonsterState monsterState;
+
     public NavMeshAgent navigation;
 
     public Animator animator;
 
 
-    //public Monster(Transform t)                 //생성자
-    //{
-    //    position = t;
-
-    //}
 
 }
---
+++ Team Project/Assets/MyTeam/Scripts/InGame/Enemy/warrior/EnemyWarrior.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyWarrior : MonoBehaviour
+{
+    private Monster e_Warrior;          //몬스터 클래스
+    private bool targeting;             //타겟 조준
+
+    public Transform target;
+
+
+    private void Start()
+    {
+       
+        e_Warrior = new Monster();
+        setting();
+    }
+
+    private void setting()
+    {
+        e_Warrior.monsterState = State.MonsterState.M_Idle;
+        e_Warrior.navigation = GetComponent<NavMeshAgent>();
+        e_Warrior.animator = GetComponent<Animator>();
+        e_Warrior.movespeed = 8.0f;
+        e_Warrior.attack_aware_distance = 4.0f;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            e_Warrior.monsterState = State.MonsterState.M_Move;
+        }
+        switch (e_Warrior.monsterState)
+        {
+            case State.MonsterState.M_Idle:
+                M_Idle();
+                break;
+            case State.MonsterState.M_Move:
+                M_Move();
+                break;
+            case State.MonsterState.M_Dead:
+                break;
+            case State.MonsterState.M_Groar:
+                break;
+            case State.MonsterState.M_Attack:
+                break;
+            case State.MonsterState.M_Return:
+                break;
+            case State.MonsterState.M_Damage:
+                break;
+        }
+    }
+
+    private void M_Move()
+    {
+        e_Warrior.animator.SetTrigger("Run");
+        
+    }
+
+    private void M_Idle()
+    {
+        e_Warrior.animator.SetTrigger("Idle");
+    }
+
+    public void OnTargetingEvent()
+    {
+
+    }
+}
