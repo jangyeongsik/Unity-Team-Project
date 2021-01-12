c2f9cae86f6effc904e43ac0996c42abf25f461e
유인 한 <gksyy123@naver.com>
2021-01-12 18:58:11 +0900

    cine_camera

---
+++ Team Project/Assets/MyTeam/Scenes/MAP/MAP006/CamerLooking.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerLooking : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        transform.LookAt(target);
+    }
+}
