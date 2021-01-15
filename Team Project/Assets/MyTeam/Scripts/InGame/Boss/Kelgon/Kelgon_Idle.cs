85043fd08a84fac3bdde5adef1308356ddab1b9d
dudqls5630 <dudqls5630@gmail.com>
2021-01-15 13:54:00 +0900

    no message

---
+++ Team Project/Assets/MyTeam/Scripts/InGame/Boss/Kelgon/Kelgon_Idle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kelgon_Idle : StateMachineBehaviour
+{
+    BossData kelgon;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (kelgon == null)
+            kelgon = animator.transform.parent.GetComponent<BossData>();
+        kelgon.bossState = State.BossState.B_Idle;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(kelgon.target != null && kelgon.bossState == State.BossState.B_Idle)
+        {
+            //트리거 두번들어가는거 예외처리
+            kelgon.bossState = State.BossState.B_Move;
+            kelgon.navigation.SetDestination(kelgon.target.position);
+            animator.SetTrigger("Walk");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
