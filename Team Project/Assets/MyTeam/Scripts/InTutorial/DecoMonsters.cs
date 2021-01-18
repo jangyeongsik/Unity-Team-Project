6303cdb8a2239fe3ac2c8097f488c71489e7340f
유인 한 <gksyy123@naver.com>
2021-01-18 10:58:08 +0900

    튜토리얼 전용 몬스터 + 장식용 몬스터들 튜토씬추가

---
+++ Team Project/Assets/MyTeam/Scripts/InTutorial/DecoMonsters.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoMonsters : MonoBehaviour
+{
+    private Monster viper;
+
+    private void Start()
+    {
+        viper = GetComponent<Monster>();
+        viper.position = transform;
+        viper.monsterKind = State.MonsterKind.M_Viper;
+    }
+
+    private void EnemyVipersSet()
+    {
+        viper.monsterState = State.MonsterState.M_Idle;
+        viper.animator = GetComponent<Animator>();
+    }
+}
