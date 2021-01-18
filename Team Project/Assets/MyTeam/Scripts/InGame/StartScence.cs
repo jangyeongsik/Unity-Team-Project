cf0c68d0cd12b7ab9eb42d8cc45c731138f67ed3
전영은 <wjsduddms128@gmail.com>
2021-01-18 11:00:39 +0900

    no message

---
+++ Team Project/Assets/MyTeam/Scripts/InGame/StartScence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScence : MonoBehaviour
+{
+    public void StartSence()
+    {
+        SceneManager.LoadScene("GameStartScene");
+    }
+}
