09cd511b6053957aaf6880e59afcb8e26ecc3423
전영은 <wjsduddms128@gmail.com>
2021-01-21 12:45:58 +0900

    no message

--- Team Project/Assets/MyTeam/Scripts/Tutorial/invenCheck.cs
+++ Team Project/Assets/MyTeam/Scripts/Tutorial/invenCheck.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class invenCheck : MonoBehaviour
 {
+    public GameObject box;
     public int talk_id;
     private bool isTalk;
     bool check;
@@ -12,8 +13,9 @@ public class invenCheck : MonoBehaviour
     {
         //조건 완료 후 박스 콜리이더 트리거 체크
         //제작했냐안했냐
-        if(DataManager.Instance.AllInvenData.EquipmentList.Count > 0 || DataManager.Instance.EquipInvenData.CurrentEquipmentList.Count > 0)
+        if(DataManager.Instance.EquipInvenData.CurrentEquipmentList.Count > 0)
         {
+            box.SetActive(false);
             check = true;
             gameObject.SetActive(false);
 
