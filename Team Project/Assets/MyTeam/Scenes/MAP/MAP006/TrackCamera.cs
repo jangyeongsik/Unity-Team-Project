318e15300995af2258bc44961dad7878c8bdd711
전영은 <wjsduddms128@gmail.com>
2021-01-28 11:12:20 +0900

    no message

--- Team Project/Assets/MyTeam/Scenes/MAP/MAP006/TrackCamera.cs
+++ Team Project/Assets/MyTeam/Scenes/MAP/MAP006/TrackCamera.cs
@@ -6,19 +6,17 @@ public class TrackCamera : MonoBehaviour
 {
     private float cameraMovingCount = 0.0f;
     private float cameraMovingDelay = 5.0f;
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
+        if (GameData.Instance.player.bossClear == false)
+        {
             cameraMovingCount += Time.deltaTime;
             if (cameraMovingCount > cameraMovingDelay)
             {
                 gameObject.SetActive(false);
                 cameraMovingCount = 0.0f;
             }
+        }
     }
 }
